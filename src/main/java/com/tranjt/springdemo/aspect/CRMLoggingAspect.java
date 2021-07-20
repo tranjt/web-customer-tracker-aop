f41b6dc6564765c6266f5fba0a4dbfec987a1bb9
JTTran <tranjt80@gmail.com>
2021-07-20 21:11:25 +0300

    Setup pointcut declarations and add @Before advice

--- src/main/java/com/tranjt/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/tranjt/springdemo/aspect/CRMLoggingAspect.java
@@ -2,10 +2,11 @@ package com.tranjt.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-
-
 
 @Aspect
 @Component
@@ -13,10 +14,36 @@ public class CRMLoggingAspect {
 
 	// setup logger
 	private Logger myLogger = Logger.getLogger(getClass().getName());
-	
+
 	// setup pointcut declarations
-	
+	@Pointcut("execution(* com.tranjt.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {
+	}
+
+	// do the same for service and dao
+	@Pointcut("execution(* com.tranjt.springdemo.service.*.*(..))")
+	private void forServicePackage() {
+	}
+
+	@Pointcut("execution(* com.tranjt.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {
+	}
+
+	@Pointcut(" forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {
+	}
+
 	// add @Before advice
-	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>> in @Before: calling method: " +  theMethod);
+		
+		// display the arguments to the method
+		
+	}
+
 	// add @AfterReturning advice
 }
