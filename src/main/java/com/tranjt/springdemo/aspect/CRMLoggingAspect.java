a180ed2f9663b73708326e885ae8af9cc6338301
JTTran <tranjt80@gmail.com>
2021-07-20 21:22:19 +0300

    Display args to method @Before, add @AfterReturn advice, display result

--- src/main/java/com/tranjt/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/tranjt/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.tranjt.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -41,9 +42,32 @@ public class CRMLoggingAspect {
 		String theMethod = theJoinPoint.getSignature().toShortString();
 		myLogger.info("====>> in @Before: calling method: " +  theMethod);
 		
-		// display the arguments to the method
+		// display the arguments to the method		
+		
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop thru and display args
+		for(Object tempArg : args) {
+			myLogger.info("====>> argument: " + tempArg);
+		}
 		
 	}
 
 	// add @AfterReturning advice
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		// display method we are returning from 
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>> in @@AfterReturning: from method: " +  theMethod);
+				
+		
+		// display data returned
+		myLogger.info("====> result: " + theResult );
+		
+	}
 }
